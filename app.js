538f09f9450db592ad9fa4a6e1ee741b0ce7e48d
AndrewKloter <31974322+AndrewKloter@users.noreply.github.com>
2026-04-17 19:20:36 -0400

    Update app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+//You then use JavaScript to instantiate InstantSearch and attach widgets to your HTML containers
+//obtained from Algolia documentation
+
+const search = instantsearch({
+  indexName: 'products',
+  searchClient: algoliasearch('YourAppID', 'YourSearchOnlyAPIKey'),
+});
+
+search.addWidgets([
+  searchBox({ container: '#searchbox' }),
+  hits({ container: '#hits' }),
+  pagination({ container: '#pagination' }),
+]);
+
+search.start();
